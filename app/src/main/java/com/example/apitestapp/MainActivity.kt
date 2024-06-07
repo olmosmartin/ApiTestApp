40a2cc4c7ae0b3a8b5d99d17b5f79a0dec17b4a5
martin olmos <martino@epidataconsulting.com>
2024-06-06 22:16:37 -0300

    obtengo query del buscador

--- app/src/main/java/com/example/apitestapp/MainActivity.kt
+++ app/src/main/java/com/example/apitestapp/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.example.apitestapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.apitestapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding //nombre de la clase con activity al inicio y binding al final
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.svBuscador.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            //cambia cada vez que vamos escribiendo
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    private fun searchByName(query: String) {
+        Log.i("tag", "searchByName: $query")
     }
 }
